6b6d7613e76375372fee00111dd4c3dfd10322e0
Tom Cannon <tcanno20@wgu.edu>
2026-03-30 22:01:02 -0700

    Replace expo-av with expo-audio for SDK 54 compatibility

--- utils/feedback.js
+++ utils/feedback.js
@@ -1,46 +1,39 @@
 import * as Haptics from 'expo-haptics';
-import { Audio } from 'expo-av';
+import { createAudioPlayer } from 'expo-audio';
 
-let clickSound = null;
-let completeSound = null;
+let clickPlayer = null;
+let completePlayer = null;
 
 async function loadSounds() {
-  if (!clickSound) {
-    const { sound } = await Audio.Sound.createAsync(require('../assets/click.wav'));
-    clickSound = sound;
-  }
-  if (!completeSound) {
-    const { sound } = await Audio.Sound.createAsync(require('../assets/complete.wav'));
-    completeSound = sound;
-  }
+  try {
+    if (!clickPlayer) clickPlayer = createAudioPlayer(require('../assets/click.wav'));
+    if (!completePlayer) completePlayer = createAudioPlayer(require('../assets/complete.wav'));
+  } catch {}
 }
 
-loadSounds().catch(() => {});
+loadSounds();
 
 export async function tapFeedback() {
   try {
-    await Promise.all([
-      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light),
-      clickSound?.replayAsync(),
-    ]);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    clickPlayer?.seekTo(0);
+    clickPlayer?.play();
   } catch {}
 }
 
 export async function selectFeedback() {
   try {
-    await Promise.all([
-      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium),
-      clickSound?.replayAsync(),
-    ]);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+    clickPlayer?.seekTo(0);
+    clickPlayer?.play();
   } catch {}
 }
 
 export async function successFeedback() {
   try {
-    await Promise.all([
-      Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success),
-      completeSound?.replayAsync(),
-    ]);
+    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+    completePlayer?.seekTo(0);
+    completePlayer?.play();
   } catch {}
 }
 
